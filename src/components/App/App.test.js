ad703d04661ed216b619f8bddb0fc66926836bf2
Yosevu Kilonzo <yosevuk@gmail.com>
2017-09-29 09:51:31 -0500

    Eject create-react-app - Add test setup

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// console.log(saveEntry);
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
-
-
